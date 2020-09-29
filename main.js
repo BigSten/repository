51177401756e059e491dcbea575729241a003797
BigSten <coolgirl25rus@mail.ru>
2020-09-29 15:12:59 +1000

    first commit

---
+++ main.js
@@ -0,0 +1,64 @@
+var token = "6812b92117d78474cddeb352fc91e631653c6c1f"; //API-ключ
+
+function join(arr) {
+  var separator = arguments.length > 1 ? arguments[1] : ", ";
+  return arr.filter(function(n){return n}).join(separator);
+}
+
+function formatCity(suggestion) {
+  var address = suggestion.data;
+  if (address.city_with_type === address.region_with_type) {
+      return address.settlement_with_type || "";
+    } else {
+      return join([
+        address.city_with_type,
+        address.settlement_with_type]);
+    }
+}
+
+var type  = "ADDRESS";
+var $region = $("#region");
+var $city   = $("#city");
+var $street = $("#street");
+var $house  = $("#house");
+
+// регион и район
+$region.suggestions({
+  token: token,
+  type: type,
+  hint: false, // 'Выберите вариант или продолжите ввод'
+  bounds: "region-area"
+});
+
+// город и населенный пункт
+$city.suggestions({
+  token: token,
+  type: type,
+  hint: false,
+  bounds: "city-settlement",
+  constraints: $region,
+  formatSelected: formatCity
+});
+
+// улица
+$street.suggestions({
+  token: token,
+  type: type,
+  hint: false,
+  bounds: "street",
+  constraints: $city,
+  count: 15
+});
+
+// дом
+$house.suggestions({
+  token: token,
+  type: type,
+  hint: false,
+  noSuggestionsHint: false,
+  bounds: "house",
+  constraints: $street
+});
+
+
+console.log($house.suggestions())
